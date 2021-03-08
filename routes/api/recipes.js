6497c7796a941c7de98e66c72ca75a27446c5107
Juan Brito <juan.a.brito90@gmail.com>
2021-03-08 02:41:38 -0300

    Updated endpoints

--- routes/api/recipes.js
+++ routes/api/recipes.js
@@ -2,7 +2,6 @@ var router = require('express').Router();
 var mongoose = require('mongoose');
 const { ObjectID } = require( 'mongodb' )
 var Recipe = mongoose.model('Recipe');
-var RecipeIngredient = mongoose.model('RecipeIngredient');
 
 router.param('recipe', function(req, res, next, id) {
   Recipe.findById(id)
@@ -19,17 +18,9 @@ router.get('/', async function(req, res, next) {
   var query = {};
   
   if(req.query.term !== 'undefined'){
-    var queryForIngredients = { 'name' : { '$regex': req.query.term, "$options": "i" } };
-
-    var ingredients = await RecipeIngredient.find(queryForIngredients).exec();
-    var ingredientsIds = ingredients.map(function(ingredient){
-      //return new ObjectID(ingredient.id);
-      return ingredient.id;
-    })
-
     query = { $or:[ 
       { 'title' : { '$regex': req.query.term, "$options": "i" } },
-      { 'ingredientsPerPerson' : { '$in': ingredientsIds } } 
+      { 'ingredientsPerPerson.name' : { '$regex': req.query.term, "$options": "i" } } 
     ] };
   }
 
@@ -65,19 +56,17 @@ router.put('/:recipe/stars', function(req, res, next) {
 });
 
 router.get('/:recipe/ingredients', function(req, res, next){
-  return req.recipe.populate('ingredientsPerPerson').execPopulate().then(async function(recipe) {
-        var ingredientsPerPerson = await Promise.all(recipe.populated('ingredientsPerPerson').map(async function(ingredientPerPersonObjectId){
-          var a = await RecipeIngredient.findById(ingredientPerPersonObjectId.toString());
-
-          return await RecipeIngredient.findById(ingredientPerPersonObjectId.toString());
-        }));
-
-      return res.json({recipeIngredients: ingredientsPerPerson.map(function(ingredientPerPerson){  
-          ingredientPerPerson.amount = ingredientPerPerson.amount * (req.query.dinersCount !== 'undefined' ? req.query.dinersCount : 1);
-
-          return ingredientPerPerson.toJSONFor();
-      })});
-  }).catch(next);
+  return res.json(
+    {
+      recipeIngredients: req.recipe.ingredientsPerPerson.map(function(ingredientPerPerson) {  
+        return {
+          name: ingredientPerPerson.name,
+          amountUnit: ingredientPerPerson.amountUnit,
+          amount: ingredientPerPerson.amount = ingredientPerPerson.amount * (req.query.dinersCount !== 'undefined' ? req.query.dinersCount : 1)
+        };
+      })
+    }
+  )
 });
 
 router.get('/:recipe/steps', function(req, res, next){
